759b18e5c5e4b211f34903af23b78450d60dc4ed
syedalyan18 <syedaliyan2244@gmail.com>
2025-09-18 18:21:58 +0100

    Add files via upload

---
+++ Q4.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+int main(){
+    int card1,card2,card3,card4,card5;
+    int two,three;
+    int c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0,c8=0,c9=0,c10=0,c11=0,c12=0,c13=0;
+    printf("enter card 1:");
+    scanf("%d",&card1);
+     printf("enter card 2:");
+    scanf("%d",&card2);
+     printf("enter card 3:");
+    scanf("%d",&card3);
+     printf("enter card 4:");
+    scanf("%d",&card4);
+     printf("enter card 5:");
+    scanf("%d",&card5);
+    if(card1==1){c1++;}
+    if(card1==2){c2++;}
+    if(card1==3){c3++;}
+    if(card1==4){c4++;}
+    if(card1==5){c5++;}
+    if(card1==6){c6++;}
+    if(card1==7){c7++;}
+    if(card1==8){c8++;}
+    if(card1==9){c9++;}
+    if(card1==10){c10++;}
+    if(card1==11){c11++;}
+    if(card1==12){c12++;}
+    if(card1==13){c13++;}
+    
+    if(card2==1){c1++;}
+    if(card2==2){c2++;}
+    if(card2==3){c3++;}
+    if(card2==4){c4++;}
+    if(card2==5){c5++;}
+    if(card2==6){c6++;}
+    if(card2==7){c7++;}
+    if(card2==8){c8++;}
+    if(card2==9){c9++;}
+    if(card2==10){c10++;}
+    if(card2==11){c11++;}
+    if(card2==12){c12++;}
+    if(card2==13){c13++;}
+    
+    if(card3==1){c1++;}
+    if(card3==2){c2++;}
+    if(card3==3){c3++;}
+    if(card3==4){c4++;}
+    if(card3==5){c5++;}
+    if(card3==6){c6++;}
+    if(card3==7){c7++;}
+    if(card3==8){c8++;}
+    if(card3==9){c9++;}
+    if(card3==10){c10++;}
+    if(card3==11){c11++;}
+    if(card3==12){c12++;}
+    if(card3==13){c13++;}
+    
+    if(card4==1){c1++;}
+    if(card4==2){c2++;}
+    if(card4==3){c3++;}
+    if(card4==4){c4++;}
+    if(card4==5){c5++;}
+    if(card4==6){c6++;}
+    if(card4==7){c7++;}
+    if(card4==8){c8++;}
+    if(card4==9){c9++;}
+    if(card4==10){c10++;}
+    if(card4==11){c11++;}
+    if(card4==12){c12++;}
+    if(card4==13){c13++;}
+    
+    if(card5==1){c1++;}
+    if(card5==2){c2++;}
+    if(card5==3){c3++;}
+    if(card5==4){c4++;}
+    if(card5==5){c5++;}
+    if(card5==6){c6++;}
+    if(card5==7){c7++;}
+    if(card5==8){c8++;}
+    if(card5==9){c9++;}
+    if(card5==10){c10++;}
+    if(card5==11){c11++;}
+    if(card5==12){c12++;}
+    if(card5==13){c13++;}
+    
+    if(c1==3){three=3;}
+    if(c2==3){three=3;}
+    if(c3==3){three=3;}
+    if(c4==3){three=3;}
+    if(c5==3){three=3;}
+    if(c6==3){three=3;}
+    if(c7==3){three=3;}
+    if(c8==3){three=3;}
+    if(c9==3){three=3;}
+    if(c10==3){three=3;}
+    if(c11==3){three=3;}
+    if(c12==3){three=3;}
+    if(c13==3){three=3;}
+    
+    
+    if(c1==2){two=2;}
+    if(c2==2){two=2;}
+    if(c3==2){two=2;}
+    if(c4==2){two=2;}
+    if(c5==2){two=2;}
+    if(c6==2){two=2;}
+    if(c7==2){two=2;}
+    if(c8==2){two=2;}
+    if(c9==2){two=2;}
+    if(c10==2){two=2;}
+    if(c11==2){two=2;}
+    if(c12==2){two=2;}
+    if(c13==2){two=2;}
+    if(two==2 && three==3){
+              printf("house full");
+              }
+          else{
+               printf("not a full house");  
+            return 0;             }    
+    }
